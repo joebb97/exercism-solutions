8eecaa6a1ab15b731bf6a1470b05d58e4461e79d
Joseph Buiteweg <joebb@umich.edu>
2021-02-11 13:41:46 -0500

    remove prune matches, very pretty

--- rust/word-count/src/lib.rs
+++ rust/word-count/src/lib.rs
@@ -1,19 +1,5 @@
 use std::collections::HashMap;
 
-pub fn prune_word(word: &str) -> String {
-    let as_string = if word.starts_with("'") {
-        &word[1..]
-    } else {
-        word
-    };
-    let as_string = if as_string.ends_with("'") {
-        &as_string[0..as_string.len() - 1]
-    } else {
-        as_string
-    };
-    as_string.to_string().to_lowercase()
-}
-
 /// Count occurrences of words.
 pub fn word_count(words: &str) -> HashMap<String, u32> {
     let counts: HashMap<String, u32> = HashMap::new();
@@ -29,7 +15,7 @@ pub fn word_count(words: &str) -> HashMap<String, u32> {
         .collect::<String>()
         .split_whitespace()
         .fold(counts, |mut cur_dict, word| {
-            let pruned = prune_word(word);
+            let pruned = word.trim_matches('\'').to_string().to_lowercase();
             let entry = cur_dict.entry(pruned).or_insert(0);
             *entry += 1;
             cur_dict
