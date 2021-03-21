246670ec590bd2966f7b07414aca662dcaec794d
Joseph Buiteweg <joebb@umich.edu>
2021-03-21 13:25:16 -0400

    Fewer lines!!

--- rust/nucleotide-count/src/lib.rs
+++ rust/nucleotide-count/src/lib.rs
@@ -9,19 +9,16 @@ fn good_char(nucleotide: char) -> bool {
 
 pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
     if !good_char(nucleotide) {
-        return Err(nucleotide);
+        Err(nucleotide)
+    } else if let Some(bad_char) = dna.chars().filter(|c| !good_char(*c)).next() {
+        Err(bad_char)
+    } else {
+        Ok(dna.chars().filter(|c| *c == nucleotide).count())
     }
-    let mut bad_chars = dna.chars().filter(|c| !good_char(*c));
-    if let Some(bad_char) = bad_chars.next() {
-        return Err(bad_char)
-    }
-    Ok(dna.chars().filter(|c| *c == nucleotide).count())
 }
 
 pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
-    let map: HashMap<char, usize> = HashMap::new();
-    let tides = "GCAT";
-    tides.chars().fold(Ok(map), |mut res, c| {
+    "GCAT".chars().fold(Ok(HashMap::new()), |mut res, c| {
         let thecount = count(c, dna);
         if res.is_err() {
             res
