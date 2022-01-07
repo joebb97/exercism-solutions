b0630d19905295a1e57719d35bfa7af6172a144c
Joseph Buiteweg <joebb@umich.edu>
2022-01-06 19:00:13 -0500

    Haskell is great!

--- haskell/hamming/src/Hamming.hs
+++ haskell/hamming/src/Hamming.hs
@@ -1,4 +1,12 @@
 module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
-distance xs ys = error "You need to implement this function."
+distance xs ys  
+    | length xs /= length ys = Nothing
+    | otherwise = Just total
+    where
+    total = sum comparisons
+    cmpc (c1, c2) = fromEnum (c1 /= c2)
+    pairings = zip xs ys
+    comparisons = map cmpc pairings
+
